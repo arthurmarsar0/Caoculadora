e6e256df08fa86c94e8b99ee6a4184a5b34b05b5
Arthur Marsaro <aam4@cin.ufpe.br>
2024-05-07 17:39:05 -0300

    Adiciona caixas de textos e descricao

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var years: Int? = nil
+    @State var months: Int? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Qual idade do seu cão?")
+                .font(.system(size: 24))
+            Text("Anos")
+            TextField("Digite quantos anos tem o seu cão.", value: $years, format: .number)
+            
+            Text("Meses")
+            TextField("Digite quantos meses tem o seu cão.", value: $months, format: .number)
         }
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
+        .bold()
+        .fontDesign(.rounded)
         .padding()
     }
 }
