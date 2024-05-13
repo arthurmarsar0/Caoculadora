9fa0c7500b1888f30f9b82a610ca4469e29bd856
Arthur Marsaro <aam4@cin.ufpe.br>
2024-05-13 17:10:03 -0300

    Finaliza jogo

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -14,78 +14,111 @@ struct ContentView: View {
     @State var months: Int? = nil
     @State var result: Int?
     @State var porteSelecionado: Porte = .pequeno
+    @State var failedInput: Bool = false
+    let tituloPreencherCampos = "Preencha os campos para cãocular"
+    @State var isZero: Bool = false
+    let tituloZero = "Prencha com valores maior que zero"
     
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Qual idade do seu cão?")
-                .font(.header5)
-            Text("Anos")
-                .font(.body1)
-            TextField("Digite quantos anos tem o seu cão.", value: $years, format: .number)
-            Text("Meses")
-                .font(.body1)
-            TextField("Digite quantos meses tem o seu cão.", value: $months, format: .number)
-            
-            Text("Porte")
-            Picker("Porte", selection: $porteSelecionado){
-                ForEach(Porte.allCases, id: \.self) {porte in
-                    Text(porte.rawValue.capitalized).tag(porte)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20.0 ) {
+                    Text("Qual idade do seu cão?")
+                        .font(.header5)
+                        .padding(.top, 24.0)
+                    VStack(alignment: .leading, spacing: 8.0){
+                        Text("Anos")
+                            .font(.body1)
+                        TextField("Digite quantos anos tem o seu cão.", value: $years, format: .number)
+                    }
+                    VStack(alignment: .leading, spacing: 8.0){
+                        Text("Meses")
+                            .font(.body1)
+                        TextField("Digite quantos meses tem o seu cão.", value: $months, format: .number)
+                    }
+                    VStack(alignment: .leading, spacing: 8.0){
+                        Text("Porte")
+                        Picker("Porte", selection: $porteSelecionado){
+                            ForEach(Porte.allCases, id: \.self) {porte in
+                                Text(porte.rawValue.capitalized).tag(porte)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    Divider()
+                    Spacer()
+                    //segment controll
+                    if let result{
+                            Text("Seu cachorro tem, em idade humana...")
+                                .font(.body1)
+                                .frame(maxWidth: .infinity)
+                            Text("\(result) ano(s)")
+                            .contentTransition(.numericText())
+                                .font(.display)
+                                .frame(maxWidth: .infinity)
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: .infinity)
+                            .shadow(radius: 20)
+                    }
+                    Spacer()
+                    Button(action: processYears, label: {
+                        ZStack{
+                            Color.pink
+                            Text("Cãocular")
+                                .foregroundStyle(.white)
+                                .font(.body1)
+                        }
+                        .cornerRadius(10)
+                    })
+                    .frame(height: 50)
+                    
                 }
-            }
-            .pickerStyle(.segmented)
-            .padding()
-            Divider()
-            Spacer()
-            //segment controll
-            if let result{
-                Text("Seu cachorro tem, em idade humana...")
-                    .font(.body1)
-                Text("\(result) anos")
-                    .font(.display)
-            } else {
-                Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                    .frame(maxWidth: .infinity)
-                    .shadow(radius: 20)
-            }
-            Spacer()
-            Button(action: processYears, label: {
-                ZStack{
-                    Color.pink
-                    Text("Calcular")
-                        .foregroundStyle(.white)
-                        .font(.body1)
+                .alert(tituloPreencherCampos, isPresented: $failedInput){
+                    Button("OK", role: .cancel, action: {})
                 }
-                .cornerRadius(10)
-            })
-            .frame(height: 50)
+                .alert(tituloZero, isPresented: $isZero){
+                    Button("OK", role: .cancel, action: {})
+                }
+                .containerRelativeFrame(.vertical)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .bold()
+                .fontDesign(.rounded)
+                .padding()
+                .navigationTitle("Cãoculadora")
+                .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarBackground(Color.pink, for: .navigationBar)
+            .toolbarColorScheme(.dark, for: .navigationBar, .tabBar)
             
+            }
         }
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .bold()
-        .fontDesign(.rounded)
-        .padding()
+        .scrollDismissesKeyboard(.immediately)
     }
     
     func processYears() {
         guard let years, let months else {
             print("prencha o campo de entrada")
+            failedInput = true
             return
         }
         
         guard years > 0 || months > 0 else {
             print("algum campo tem que ter valor maior que 0")
+            isZero = true
             return
         }
         
-        result = porteSelecionado.conversaoIdade(
-            anos: years,
-            meses: months
-        )
+        withAnimation(.easeIn.speed(0.5)){
+            result = porteSelecionado.conversaoIdade(
+                anos: years,
+                meses: months
+            )
+        }
     }
 }
 
